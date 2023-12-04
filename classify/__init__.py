e5fe6d3de85d7c5d101c98feca8cf6536adbd58f
Sanna Lun <tin.lun@aalto.fi>
2023-12-04 16:38:27 +0200

    add mlp

--- classify/__init__.py
+++ classify/__init__.py
@@ -1,5 +1,5 @@
 #!/usr/bin/env python3
-from .svm import classifier
+from .models import classifier
 from .compute_metrics import compute_metrics
 
 __all__ = ["classifier", "compute_metrics"]
--- classify/models.py
+++ classify/models.py
@@ -2,6 +2,7 @@
 from sklearn.pipeline import make_pipeline
 from sklearn.preprocessing import StandardScaler
 from sklearn.svm import SVC
+from sklearn.neural_network import MLPClassifier
 from .compute_metrics import compute_metrics
 import time 
 
@@ -29,18 +30,24 @@ def classifier(X, y, X_test, y_test) -> SVC:
         {'uar': 0.720959595959596, 'f1': 0.628428927680798, 
         'macro_f1': 0.7195511855684803, 'accuracy': 0.7491582491582491}
     """
+    svm = SVC(kernel="linear", random_state=2023)
+    mlp = MLPClassifier(hidden_layer_sizes=(10,), 
+                        random_state=2023,
+                        max_iter=1000)
     
-    print("Start training SVM.")
-    start = time.time()
-    clf = make_pipeline(StandardScaler(), SVC(kernel="linear", random_state=2023))
-    clf.fit(X, y)
-    end = time.time()
-    print(f"Training done. Time taken: {(end-start)/60:.2f} mins.")
-    
-    print("Evaluation on test set:")
-    y_pred = clf.predict(X_test)
-    metrics = compute_metrics(y_test, y_pred)
-    print(metrics)
+    for name, model in zip(["SVM", "MLP"], [svm, mlp]):
+        
+        print(f"---Start training {name}---")
+        start = time.time()
+        clf = make_pipeline(StandardScaler(), model)
+        clf.fit(X, y)
+        end = time.time()
+        print(f"Training done. Time taken: {(end-start)/60:.2f} mins.")
+        
+        print("Evaluation on test set:")
+        y_pred = clf.predict(X_test)
+        metrics = compute_metrics(y_test, y_pred)
+        print(metrics)
     
     return clf 
-    
+
